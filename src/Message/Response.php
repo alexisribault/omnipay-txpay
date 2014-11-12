05e3a4c8198ca363f9c5046598e4c9bb7e8a3969
alexis <fenix@gmail.com>
2014-11-12 16:45:45 +1000

    test done

--- src/Message/Response.php
+++ src/Message/Response.php
@@ -11,7 +11,7 @@ class Response extends AbstractResponse
 {
     public function isSuccessful()
     {
-        return $this->data;
+        return false;//$this->data;
     }
 
     public function getMessage()
